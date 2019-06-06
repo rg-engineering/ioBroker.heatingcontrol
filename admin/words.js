eb66a00e722180a5407f4ce4d0c02111be40c693
Rene Glaß <info@rg-engineering.eu>
2019-06-06 21:29:36 +0200

    translations

--- admin/words.js
+++ admin/words.js
@@ -2,8 +2,384 @@
 'use strict';
 
 systemDictionary = {
+    "Main settings":
+    {
+        "en": "Main settings",
+        "de": "Haupteinstellungen",
+        "pt": "Configurações principais",
+        "nl": "Belangrijkste instellingen",
+        "fr": "Réglages principaux",
+        "it": "Impostazioni principali",
+        "es": "Ajustes principales",
+        "pl": "Ustawienia główne",
+        "ru": "Основные параметры",
+        "zh-cn": "主要设置"
+    },
+    "profile":
+    {
+        "en": "profile",
+        "de": "Profil",
+        "ru": "профиль",
+        "pt": "perfil",
+        "nl": "profiel",
+        "fr": "profil",
+        "it": "profilo",
+        "es": "perfil",
+        "pl": "profil",
+        "zh-cn": "轮廓"
+    },
+    "devices":
+    {
+        "en": "devices",
+        "de": "Geräte",
+        "pt": "dispositivos",
+        "nl": "apparaten",
+        "fr": "dispositifs",
+        "it": "dispositivi",
+        "es": "dispositivos",
+        "pl": "pomysłowość",
+        "zh-cn": "设备",
+        "ru": "приборы"
+    },
+    "Gewerk":
+    {
+        "en": "function",
+        "de": "Funktion",
+        "zh-cn": "功能",
+        "ru": "функция",
+        "pt": "função",
+        "nl": "functie",
+        "fr": "une fonction",
+        "it": "funzione",
+        "es": "función",
+        "pl": "funkcjonować"
+    },
+    "PathThermostats":
+    {
+        "en": "Path to Thermostats",
+        "ru": "Путь к термостатам",
+        "de": "Pfad zu den Thermostaten",
+        "pt": "Caminho para termostatos",
+        "nl": "Pad naar thermostaten",
+        "fr": "Chemin des thermostats",
+        "es": "Camino a los termostatos",
+        "it": "Percorso per termostati",
+        "pl": "Ścieżka do termostatów",
+        "zh-cn": "恒温器之路"
+    },
+    "PathActors":
+    {
+        "en": "Path to actuators",
+        "de": "Pfad zu den Aktoren",
+        "ru": "Путь к приводам",
+        "pt": "Caminho para os atuadores",
+        "nl": "Pad naar actuatoren",
+        "pl": "Droga do siłowników",
+        "fr": "Chemin vers les actionneurs",
+        "it": "Percorso per attuatori",
+        "es": "Camino a los actuadores",
+        "zh-cn": "执行器的路径"
+    },
+    "timezone":
+    {
+        "en": "timezone",
+        "de": "Zeitzone",
+        "ru": "часовой пояс",
+        "pt": "fuso horário",
+        "nl": "tijdzone",
+        "fr": "fuseau horaire",
+        "it": "fuso orario",
+        "es": "zona horaria",
+        "pl": "strefa czasowa",
+        "zh-cn": "时区"
+    },
+    "deleteall":
+    {
+        "en": "delete all",
+        "de": "alles löschen",
+        "ru": "удалить все",
+        "nl": "Verwijder alles",
+        "pt": "deletar tudo",
+        "fr": "effacer tout",
+        "it": "cancella tutto",
+        "es": "eliminar todos",
+        "pl": "Usuń wszystko",
+        "zh-cn": "删除所有"
 
+    },
+    "use_actors":
+    {
+        "en": "use actuator",
+        "ru": "использовать привод",
+        "de": "Stellantrieb verwenden",
+        "es": "usar actuador",
+        "pl": "użyj siłownika",
+        "pt": "usar atuador",
+        "nl": "gebruik de actuator",
+        "fr": "utiliser actionneur",
+        "zh-cn": "使用执行器",
+        "it": "utilizzare l'attuatore"
+    },
+    "UseActorsIfNotHeating":
+    {
+        "en": "use actuators if no heating period",
+        "de": "Verwendung der Stellantriebe, wenn keine Heizperiode vorliegt",
+        "ru": "используйте приводы, если нет периода нагрева",
+        "it": "utilizzare gli attuatori se non ci sono periodi di riscaldamento",
+        "fr": "utiliser des actionneurs si aucune période de chauffage",
+        "pt": "usar atuadores se não houver período de aquecimento",
+        "nl": "gebruik actuators als er geen verwarmingsperiode is",
+        "pl": "używać siłowników, jeśli nie ma okresu ogrzewania",
+        "zh-cn": "如果没有加热期，则使用执行器",
+        "es": "usar actuadores si no hay periodo de calentamiento"
+    },
+    "UseActorsIfNoThermostat":
+    {
+        "en": "use actuators if no thermostat available",
+        "ru": "используйте приводы, если термостат недоступен",
+        "de": "Verwendung der Stellantriebe, wenn kein Thermostat vorhanden ist",
+        "pt": "use atuadores se não houver termostato disponível",
+        "nl": "gebruik actuators als er geen thermostaat beschikbaar is",
+        "fr": "utiliser des actionneurs si aucun thermostat n'est disponible",
+        "it": "utilizzare gli attuatori se non è disponibile un termostato",
+        "es": "usar actuadores si no hay termostato disponible",
+        "pl": "używać siłowników, jeśli termostat nie jest dostępny",
+        "zh-cn": "如果没有恒温器，请使用执行器"
+    },
+    "ProfileType":
+    {
+        "en": "Profile Type",
+        "de": "Profiltyp",
+        "ru": "Тип профиля",
+        "pt": "Tipo de perfil",
+        "nl": "Profieltype",
+        "fr": "Type de profil",
+        "it": "Tipo di profilo",
+        "es": "Tipo de perfil",
+        "pl": "Typ profilu",
+        "zh-cn": "档案类型"
+    },
+    "NumberOfProfiles":
+    {
+        "en": "Number Of profiles",
+        "de": "Anzahl der Profile",
+        "fr": "Nombre de profils",
+        "it": "Numero di profili",
+        "es": "Número de perfiles",
+        "pl": "Liczba profili",
+        "zh-cn": "配置文件数量",
+        "ru": "Количество профилей",
+        "pt": "Número de perfis",
+        "nl": "Aantal profielen"
+    },
+    "NumberOfPeriods":
+    {
+        "en": "Number Of Periods",
+        "ru": "Количество периодов",
+        "de": "Anzahl der Perioden",
+        "pt": "Número de Períodos",
+        "nl": "Aantal perioden",
+        "fr": "Nombre de périodes",
+        "it": "Numero di periodi",
+        "es": "Numero de periodos",
+        "pl": "Liczba okresów",
+        "zh-cn": "期数"
+    },
+    "Feiertag":
+    {
+        "en": "public holiday",
+        "de": "Feiertag",
+        "ru": "праздник",
+        "pt": "feriado",
+        "it": "festa nazionale",
+        "es": "día festivo",
+        "pl": "święto państwowe",
+        "zh-cn": "公共假期",
+        "nl": "wettelijke feestdag",
+        "fr": "jour férié"
+    },
+    "Search":
+    {
+        "en": "Search",
+        "de": "Suche",
+        "ru": "Поиск",
+        "pt": "Procurar",
+        "nl": "Zoeken",
+        "fr": "Chercher",
+        "it": "Ricerca",
+        "es": "Buscar",
+        "zh-cn": "搜索",
+        "pl": "Szukaj"
+    },
+    "room":
+    {
+        "en": "room",
+        "de": "Zimmer",
+        "ru": "номер",
+        "pt": "sala",
+        "nl": "kamer",
+        "fr": "pièce",
+        "it": "camera",
+        "es": "habitación",
+        "pl": "sala",
+        "zh-cn": "房间"
+    },
+    "thermostat":
+    {
+        "en": "thermostat",
+        "de": "Thermostat",
+        "zh-cn": "恒温器",
+        "it": "termostato",
+        "es": "termostato",
+        "ru": "термостат",
+        "pt": "termostato",
+        "nl": "thermostaat",
+        "fr": "thermostat",
+        "pl": "termostat"
+    },
+    "type":
+    {
+        "en": "type",
+        "de": "Type",
+        "ru": "тип",
+        "pt": "tipo",
+        "nl": "type",
+        "fr": "type",
+        "it": "genere",
+        "es": "tipo",
+        "pl": "rodzaj",
+        "zh-cn": "类型"
+    },
+    "typeID":
+    {
+        "en": "type ID",
+        "de": "Typ-ID",
+        "ru": "тип ID",
+        "nl": "type ID",
+        "pt": "ID do tipo",
+        "fr": "ID de type",
+        "it": "tipo ID",
+        "es": "ID de tipo",
+        "pl": "wpisz ID",
+        "zh-cn": "类型ID"
+    },
+    "actor1":
+    {
+        "en": "actuator 1",
+        "de": "Stellantrieb 1",
+        "ru": "привод 1",
+        "pt": "atuador 1",
+        "fr": "actionneur 1",
+        "nl": "actuator 1",
+        "it": "attuatore 1",
+        "es": "actuador 1",
+        "pl": "siłownik 1",
+        "zh-cn": "执行器1"
+    },
+    "actor2":
+    {
+        "en": "actuator 2",
+        "de": "Stellantrieb 2",
+        "ru": "привод 2",
+        "fr": "actionneur 2",
+        "it": "attuatore 2",
+        "es": "actuador 2",
+        "pl": "siłownik 2",
+        "zh-cn": "执行器2",
+        "pt": "atuador 2",
+        "nl": "actuator 2"
+    },
+    "active":
+    {
+        "en": "active",
+        "de": "aktiv",
+        "ru": "активный",
+        "pt": "ativo",
+        "nl": "actief",
+        "it": "attivo",
+        "es": "activo",
+        "fr": "actif",
+        "pl": "aktywny",
+        "zh-cn": "活性"
+    },
+    "nothing":
+    {
+        "en": "nothing",
+        "de": "nichts",
+        "ru": "ничего такого",
+        "pt": "nada",
+        "nl": "niets",
+        "fr": "rien",
+        "it": "Niente",
+        "es": "nada",
+        "pl": "nic",
+        "zh-cn": "没有"
+    },
+    "off":
+    {
+        "en": "off",
+        "de": "aus",
+        "ru": "от",
+        "pt": "fora",
+        "nl": "uit",
+        "it": "via",
+        "es": "apagado",
+        "pl": "poza",
+        "zh-cn": "离",
+        "fr": "de"
+    },
+    "on":
+    {
+        "en": "on",
+        "de": "an",
+        "ru": "на",
+        "nl": "op",
+        "fr": "sur",
+        "it": "sopra",
+        "es": "en",
+        "pl": "na",
+        "zh-cn": "上",
+        "pt": "em"
+    },
+    "Mo-So":
+    {
+        "en": "Monday-Sonday",
+        "ru": "Понедельник-Sonday",
+        "de": "Montag-Sonntag",
+        "it": "Lunedi-Sonday",
+        "pt": "Segunda-feira-segunda-feira",
+        "es": "Lunes a domingo",
+        "pl": "Poniedziałek-poniedziałek",
+        "zh-cn": "周一Sonday",
+        "nl": "Maandag tot en met Sonday",
+        "fr": "Lundi-dimanche"
+    },
+    "Mo-Fr + Sa-So":
+    {
+        "en": "Monday-Friday + Saturday-Sonday",
+        "ru": "Понедельник-пятница + суббота-воскресенье",
+        "de": "Montag-Freitag + Samstag-Sonntag",
+        "pt": "Segunda a sexta + sábado a segunda",
+        "fr": "Lundi-vendredi + samedi-dimanche",
+        "nl": "Maandag-vrijdag + zaterdag-zondag",
+        "it": "Lunedì-venerdì + sabato-Sonday",
+        "es": "Lunes a viernes + sábado a domingo",
+        "pl": "Poniedziałek-piątek + sobota-poniedziałek",
+        "zh-cn": "周一至周五+周六 - 桑迪"
+    },
+    "every Day":
+    {
+        "en": "every Day",
+        "de": "jeden Tag",
+        "ru": "каждый день",
+        "pt": "todo dia",
+        "nl": "elke dag",
+        "fr": "tous les jours",
+        "it": "ogni giorno",
+        "es": "todos los días",
+        "pl": "codzienny",
+        "zh-cn": "每天"
+    }
 
-   
     
 };
